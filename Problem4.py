a1945c67d2074662703e24af9ab4acd85ea77625
Philipp03062005 <88972886+Philipp03062005@users.noreply.github.com>
2021-08-15 16:39:50 +0300

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -1,1 +1,10 @@
+def ispalindrome(number):
+	return number == int(str(number)[::-1])
 
+result = 0
+for num1 in range(999, 700, -1):
+	for num2 in range(999, 700, -1):
+		if ispalindrome(num1 * num2):
+			if num1 * num2 > result:
+				result = num1 * num2
+print (result)
